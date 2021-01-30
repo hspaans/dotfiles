0aeaaa71e19994187f38e8f069b82077a97eb52e
Hans Spaans <hans@dailystuff.nl>
2021-01-30 03:36:15 +0100

    Copy files instead of symlinking for Containers

--- install.sh
+++ install.sh
@@ -3,12 +3,15 @@
 set -e
 
 if which rcup >/dev/null; then
-    if [ ! -f ~/.rcrc ]; then
-        ln -s ~/dotfiles/rcrc ~/.rcrc
-    fi
     if [ -f /.dockerenv ]; then
+        if [ ! -f ~/.rcrc ]; then
+            cp ~/dotfiles/rcrc ~/.rcrc
+        fi
         rcup -C -v
     else
+        if [ ! -f ~/.rcrc ]; then
+            ln -s ~/dotfiles/rcrc ~/.rcrc
+        fi
         rcup -v
     fi
 fi

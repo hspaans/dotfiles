61183cae75e7c14c7c15e9325efec2afedbc9092
Hans Spaans <hans@dailystuff.nl>
2021-12-09 00:33:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+if ! which rcup >/dev/null; then
+    # Install `rcm` on Debian
+    if [ -f /etc/debian_version ]; then
+        sudo apt update
+        sudo apt install rcm
+    fi
+fi
+
 if which rcup >/dev/null; then
     if [ ! -f ~/.rcrc ]; then
         ln -s ~/dotfiles/rcrc ~/.rcrc

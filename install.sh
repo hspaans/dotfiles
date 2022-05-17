bc6838f8ed51e8e7b7e3dce8208c2499397f8d5c
Hans Spaans <hans@dailystuff.nl>
2022-05-17 16:09:34 +0200

    Use $(...) notation instead of legacy backticks

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ set -e
 REPOPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Check if the command exists
-[[ -x `which rcup` ]] && rcup -d "$REPOPATH" -x "README*.md LICENSE install.sh"
+[[ -x $(which rcup) ]] && rcup -d "$REPOPATH" -x "README*.md LICENSE install.sh"

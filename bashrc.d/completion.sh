51b72666cbb4fb2c37a416aee795ec77ab5123c5
Hans Spaans <hans@dailystuff.nl>
2022-05-30 23:49:15 +0200

    Use $(...) notation

--- bashrc.d/completion.sh
+++ bashrc.d/completion.sh
@@ -3,15 +3,15 @@
 #                                                                               
 # Source completion for GitHub commandline client                               
 #                                                                               
-[[ `which gh` ]] && eval "$(gh completion -s bash)"                             
+[[ $(which gh) ]] && eval "$(gh completion -s bash)"                             
                                                                                 
 #                                                                               
 # Source completion for AWS commandline client                                  
 #                                                                               
-[[ `which aws_completer` ]] && complete -C `which aws_completer` aws            
+[[ $(which aws_completer) ]] && complete -C $(which aws_completer) aws            
                                                                                 
 #                                                                               
 # Source completion for package installer for Python                            
 #                                                                               
-[[ `which pip` ]] && eval "`pip completion --bash`" 
+[[ $(which pip) ]] && eval "$(pip completion --bash)" 
 
